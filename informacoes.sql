8e43448f1b019e2fda59051eebeb07dc3b4b4b9d
eduardoabach <eduardoabach@gmail.com>
2016-07-27 15:10:51 -0300

    Novos conhecimentos.

--- informacoes.sql
+++ informacoes.sql
@@ -1,4 +1,7 @@
 
+/* Endereço ip server do banco */
+select inet_server_addr();
+
 /*Database*/
 SELECT current_database();
 
@@ -6,31 +9,31 @@ SELECT current_database();
 SELECT pg_catalog.pg_size_pretty(pg_database_size(current_database()));
 
 /*Listar bases*/
-SELECT datname FROM pg_database WHERE datistemplate = false order by datname;
+SELECT datname FROM pg_database WHERE datistemplate = false ORDER BY datname;
 
 /*Nome do schema atual*/
 show search_path;
 SELECT catalog_name FROM information_schema.information_schema_catalog_name;
-SELECT table_catalog FROM information_schema.tables limit 1;
+SELECT table_catalog FROM information_schema.tables LIMIT 1;
 
 /*Listar Schemas existentes na base*/
 SELECT table_schema 
 FROM information_schema.tables 
-where table_schema not in('information_schema', 'pg_catalog') 
-group by table_schema
-order by table_schema;
+WHERE table_schema not IN('information_schema', 'pg_catalog') 
+GROUP BY table_schema
+ORDER BY table_schema;
 
 /*Listar tabelas e seus schemas*/
 SELECT table_schema, table_name 
 FROM information_schema.tables 
-where table_type = 'BASE TABLE' and table_schema not in('information_schema', 'pg_catalog')
-order by table_schema, table_name; --table_type = 'VIEW'
+WHERE table_type = 'BASE TABLE' AND table_schema not IN('information_schema', 'pg_catalog')
+ORDER BY table_schema, table_name; --table_type = 'VIEW'
 
 /*Listar triggers*/
 SELECT 
 	event_manipulation, 
-	trigger_schema||'.'||trigger_name as where,
-	event_object_catalog||'.'||event_object_schema||'.'||event_object_table as destiny
+	trigger_schema||'.'||trigger_name AS where_cond,
+	event_object_catalog||'.'||event_object_schema||'.'||event_object_table AS destiny
 FROM information_schema.triggers;
 
 /*Listar usuários existens*/
@@ -39,42 +42,38 @@ SELECT * FROM pg_user;
 /*Listar conexões do banco e suas orígens*/
 SELECT datname,usename,client_addr,client_port FROM pg_stat_activity;
 
-/*Parâmetros*/
+/*Parâmetros, pode usar select tabela ou show*/
+select setting from pg_settings where name = 'port'
 SHOW ALL;
-SHOW log_timezone;
-SHOW TimeZone;
 SHOW port;
 SHOW search_path;
 SHOW server_encoding;
 SHOW server_version;
+SHOW data_directory; /* diretório do database*/
+SHOW log_timezone; /* Descobrir linguágem, país do database*/
+SHOW TimeZone;
+SHOW default_text_search_config;
+
+/* Usuários e possivelmente um hash md5 de senha na coluna passwd */
+select * from pg_shadow
 
 /* Campo de identificação do sistema para cada linha da tabela [ctid], útil para tabelas sem pk(sinistro) */
-select ctid,* from un_unico order by ctid;
+SELECT ctid,* FROM un_unico ORDER BY ctid;
 
 SELECT * FROM pg_user;
 SELECT * FROM pg_views;
-SELECT * FROM pg_tables order by schemaname, tablename;
+SELECT * FROM pg_tables ORDER BY schemaname, tablename;
 
 SELECT table_schema 
 FROM information_schema.tables 
-where table_schema not in('information_schema', 'pg_catalog') 
-group by table_schema
-order by table_schema;
+WHERE table_schema not IN('information_schema', 'pg_catalog') 
+GROUP BY table_schema
+ORDER BY table_schema;
 
-SELECT * FROM information_schema.tables;
-
-SELECT * FROM pg_settings;
-
---show ls
-
-SELECT * FROM information_schema.tables;
+--show...
 SELECT * FROM information_schema.sequences;
 SELECT * FROM information_schema.pg_group;
-SELECT * FROM information_schema.pg_user;
-SELECT * FROM information_schema.pg_views;
-SELECT * FROM information_schema.pg_tables;
 SELECT * FROM information_schema.pg_indexes;
-SELECT * FROM information_schema.pg_settings;
 SELECT * FROM information_schema.parameters;
 SELECT * FROM information_schema.attributes;
 
--- utilidades.sql
+++ utilidades.sql
@@ -7,17 +7,21 @@ ALTER TABLE nameschema.nametable SET SCHEMA nameschema_alter; --alterar tabela d
 SET search_path TO schema_alter;
 
 --hash de um registro da tabela
-SELECT tb.id, md5(CAST((tb.*) AS text)) FROM tb_teste AS tb limit 10;
+SELECT tb.id, md5(CAST((tb.*) AS text)) FROM tb_teste AS tb LIMIT 10;
 
 --json completo de um registro
-SELECT tb.id, row_to_json(tb) FROM tb_teste AS tb limit 10;
+SELECT tb.id, row_to_json(tb) FROM tb_teste AS tb LIMIT 10;
 
 --criar view
 CREATE VIEW nome_view AS SELECT * FROM tb_teste AS tb;
+
+--Criar outra tabela com mesma estrutura, duplicando informações da original ou não
+CREATE TABLE public.un_test as SELECT * FROM un_unico WHERE ctid is NULL;
+CREATE TABLE public.un_test as SELECT * FROM un_unico;
 
 --Deletar linhas duplicadas
 DELETE FROM nometabela WHERE ctid NOT IN
 	(SELECT max(ctid) FROM nometabela GROUP BY campoid,complementochave);
 
-
-
+/* Mostrar campo nome que não estão no group by, concatenando com , */
+SELECT string_agg(nome, ', '), count(1) FROM produtos GROUP BY categoria; 
