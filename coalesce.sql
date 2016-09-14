e2b3db5dad68770dfc04453fa0ba2d48a667b9d2
eduardoabach <eduardoabach@gmail.com>
2016-09-14 17:43:30 -0300

    Clear files.

--- coalesce.sql
+++
@@ -1,6 +0,0 @@
-
-select COALESCE(null,null,3); -- 3
-select COALESCE(4,null,1); -- 4
-select COALESCE(1,null,4); -- 1
-select COALESCE(1,null,4,null); -- 1
-select COALESCE(null,1,null,4,null); -- 1
