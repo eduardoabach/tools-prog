4030a93d1199d99f27fc347be09c3805146bdf2e
eduardoabach <eduardoabach@gmail.com>
2016-09-21 15:50:13 -0300

    Break human test calc simple eval js.

--- js/teste_humano_calc.js
+++ js/teste_humano_calc.js
@@ -1,4 +1,5 @@
 
 //neste exemplo contorna uma situação simples de validação onde o usuário deve informar o resultado de uma conta
 //está conta está em um campo, não em uma imagem: <div id='math_expression'>3 + 11 = ?</div>
+//exemplo: http://conteudo.instruct.com.br/artigo-10-coisas-que-voce-nao-sabia-sobre-gitlab
 document.getElementsByName('captcha')[0].value = eval($('#math_expression').html().replace(' = ?',''));
