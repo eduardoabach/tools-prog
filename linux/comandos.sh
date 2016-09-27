0dc3952837ad4f3ba1d6e1ac2255069729658b71
eduardoabach <eduardoabach@gmail.com>
2016-09-27 10:43:27 -0300

    Linux: comandos úteis...

---
+++ linux/comandos.sh
@@ -0,0 +1,15 @@
+
+#Informações sobre o processador, arquitetura 32/64bits, tamanho do cache, núcleos, GHz...
+lscpu
+
+#Processos ativos
+top
+
+#Mata processo com pid 1234, o 9 é para forçar
+kill 1234 -s 9
+
+#Memória ram e swap, total, uso, livre
+free -h
+
+
+
