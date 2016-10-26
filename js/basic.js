666aa1f9578e8233c9873b51c39e83820a4d0f83
eduardoabach <eduardoabach@gmail.com>
2016-10-26 14:42:32 -0200

    Javascript usos básicos.

---
+++ js/basic.js
@@ -0,0 +1,14 @@
+
+alert('test');
+
+var respostaDeletar = confirm("Deseja deletar?");
+if (respostaDeletar == true) {
+} else {
+}
+
+var idade = prompt("Qual é a sua idade?", '18');
+if(idade > 18) {
+}
+
+history.back();
+history.go(-1);   
