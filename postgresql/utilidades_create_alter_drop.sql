a44be7582da5a66e51bff9b8f8ef7bffef70af6d
eduardoabach <eduardoabach@gmail.com>
2016-11-01 16:17:42 -0200

    Create table, add column, change type column...

--- postgresql/utilidades_create_alter_drop.sql
+++ postgresql/utilidades_create_alter_drop.sql
@@ -15,3 +15,26 @@ CREATE VIEW nome_view AS SELECT * FROM tb_teste AS tb;
 CREATE TABLE public.un_test AS SELECT * FROM un_unico WHERE ctid is NULL;
 CREATE TABLE public.un_test AS SELECT * FROM un_unico;
 
+-- Criar tabela
+CREATE TABLE nome_tabela (
+    id serial,
+    id_outra_tabela integer,
+    tipo numeric(2,0) /* 00 ~ 99 */
+);
+
+ALTER TABLE nome_tabela ADD COLUMN id_outra_tabela integer references nome_outra_tabela(id);
+ALTER TABLE nome_tabela ADD COLUMN data_pericia date;
+ALTER TABLE nome_tabela ADD COLUMN observacao text;
+
+ALTER TABLE nome_tabela ADD COLUMN tipo character varying(1);
+COMMENT ON COLUMN nome_tabela.tipo IS 'Tipo: P - Pendente, C - Concluído';
+
+ALTER TABLE nome_tabela ADD CONSTRAINT nome_tabela_nome_campo_key UNIQUE (nome_campo);
+
+ALTER TABLE nome_tabela ADD CONSTRAINT nome_tabela_pkey PRIMARY KEY (id);
+
+ALTER TABLE nome_tabela ALTER COLUMN tipo SET DEFAULT 'P';
+
+ALTER TABLE nome_tabela ADD COLUMN remuneracao numeric(6,2);
+ALTER TABLE nome_tabela ALTER COLUMN remuneracao TYPE numeric(8,4);
+
