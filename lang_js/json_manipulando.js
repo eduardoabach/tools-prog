dc6374246c6e7becc14a55e7f85cf32bfee787c2
eduardoabach <eduardoabach@gmail.com>
2017-01-18 16:59:42 -0200

    Json use in javascrip and php.

--- lang_js/json_manipulando.js
+++ lang_js/json_manipulando.js
@@ -30,3 +30,20 @@ $.getJSON(url_do_json, function(dados) {
         $(document.body).append('<div>' + dados[i].titulo + ', ' + dados[i].duracao + '</div>');
     }
 });
+
+// #######################################################
+
+//verificar se existe a key no json
+if(j.hasOwnProperty('msg')){
+    alert(j.msg);
+}
+
+// #######################################################
+
+$(function(){
+    var jsonString = [{"titulo":"noticia 1","corpo":"corpo da noticia 1","data":"02\/07\/2014"},{"titulo":"noticia 2","corpo":"corpo da noticia 2","data":"02\/07\/2014"},{"titulo":"noticia 3","corpo":"corpo da noticia 3","data":"02\/07\/2014"},{"titulo":"noticia 4","corpo":"corpo da noticia 4","data":"02\/07\/2014"}];
+
+    $.each(jsonString, function(i, item){
+        $('.noticias').append("<li>"+ item.data +" - " +item.titulo + "</li>");
+    });
+});
