056a8b2f408ee331802b0978eb4e52cacc012e31
eduardoabach <eduardoabach@gmail.com>
2017-02-09 15:34:38 -0200

    Jquery init examples.

---
+++ lang_js/jquery_usos.js
@@ -0,0 +1,15 @@
+
+$('#id_form').serialize();
+
+//buscas de elementos dentro de outro
+var Form = $('#id_form');
+Form.find('#campo_a').change(function(){
+	alert('alterado');
+});
+
+//verificar se um checkbox esta marcado
+var campoMarcado = (Form.find('#id_campo_checkbox').attr('checked') == undefined);
+
+
+
+
