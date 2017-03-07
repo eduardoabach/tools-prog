b6b6372a8f739903282a192f4af1a7a700c9b1a8
eduardoabach <eduardoabach@gmail.com>
2017-03-07 10:54:36 -0300

    [Hackerrank] Pascal Triangle

---
+++ lang_php/problems solved/pascal_triangle.php
@@ -0,0 +1,56 @@
+<?php
+
+// Minha resolução, usando array da ultima linha
+// No meu posso basear o triangulo em quanquer número inicial
+$repeticoes = 15;
+$numIni = 1;
+$linhaOld = array();
+
+for($posHoriz = 1; $posHoriz <= $repeticoes; $posHoriz++){
+        $linhaAt = null;
+
+        //estrutura lateralmente triangulo
+        for($posVert = 1; $posVert <= $posHoriz; $posVert++){
+                
+                $valItem = $numIni;
+                if($posHoriz != 1){
+                        $keyArrA = $posVert-2;
+                        $keyArrB = $posVert-1;
+                        $valA = (isset($linhaOld[$keyArrA])) ? $linhaOld[$keyArrA] : 0;
+                        $valB = (isset($linhaOld[$keyArrB])) ? $linhaOld[$keyArrB] : 0;
+                        $valItem = $valA+$valB;
+                }
+                
+                // primeiro número da linha não tem espaço
+                $linhaAt .= ($linhaAt == null) ? $valItem : ' '.$valItem;
+                
+        }
+
+        echo $linhaAt.'<br>';
+        $linhaOld = explode(' ', $linhaAt);
+}
+
+?>
+
+
+
+<?php  
+ 
+// Solução que encontrei na web, matematicamente com fatoração
+// É fico com número inicial 1.
+function factorial($n){
+    return ($n < 2) ? 1 : $n * factorial($n-1);
+}
+
+function pascal($n){
+    for($posHoriz=0; $posHoriz<$n; $posHoriz++){
+        for($posVert=0; $posVert<($posHoriz+1); $posVert++){
+            echo factorial($posHoriz) / (factorial($posVert) * factorial($posHoriz - $posVert));
+            echo " ";
+        }
+        
+        echo "<br>";
+    }
+}
+pascal(15);
+?>
