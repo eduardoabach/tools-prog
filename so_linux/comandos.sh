c888f3a49caf1348a6501125dc9f236611586612
Eduardo Bach <eduardoabach@gmail.com>
2017-03-20 20:32:46 -0300

    crontab usos

--- so_linux/comandos.sh
+++ so_linux/comandos.sh
@@ -25,6 +25,17 @@ service httpd restart #centos, fedora
 # Adicionar comando ao iniciar OS, editar arquivo como preferir, usando o nano no exemplo
 nano /etc/rc.d/rc.local
 
+#Agendar operações periodicas usando o crontab 
+#seguindo a seguinte estrutura [minutos] [horas] [dias do mês] [mês] [dias da semana] [usuário] [comando] 
+#Minutos: informe números de 0 a 59
+#Horas: informe números de 0 a 23
+#Dias do mês: informe números de 1 a 31
+#Mês: informe números de 1 a 12
+#Dias da semana: informe números de 0 a 7
+#Usuário: é o usuário que vai executar o comando (não é necessário especificá-lo se o arquivo do próprio usuário) 
+#arquivo fica em /etc/crontab, ou no contexto de user em /var/spool/cron
+30 22 2,10 * * echo "Exemplo..." #A frase é exibida às 22 horas e 30 minutos, nos dias 2 e 10, em todos os meses e em todos os dias da semana. 
+
 # verificar se esta iniciando com o sistema, (Centos?)
 chkconfig httpd
 # ativar na iniciação do sistema
