9d387cbad281fa38d978c71e5f226b845ed769a9
eduardoabach <eduardoabach@gmail.com>
2017-05-04 11:40:18 -0300

    Add comandos e descricoes

--- git/comandos_cotidiano.sh
+++ git/comandos_cotidiano.sh
@@ -12,10 +12,12 @@ git pull
 git push
 
 # Outras situações
-git checkout -f
 git log
 git log --name-only
-git revert --no-eddit 2ab16512207e7b943e4dc1fd2d1176a1d4384a6a
+git checkout -f
+git checkout -f 2ab16512207e7b943e4dc1fd2d1176a1d438 # voltar todo projeto para esse momento
+git revert --no-edit 2ab16512207e7b943e4dc1fd2d1176a1d438 # desfazer a alteracao
+
 
 #ferramentas
 sudo apt install gitk
