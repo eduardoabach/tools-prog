f33a6a7caf7a75b5ff99a766647c4f452c5bee1b
eduardoabach <eduardoabach@gmail.com>
2017-05-17 16:42:09 -0300

    Postgres case to fields

--- db_postgres/ut_select.sql
+++ db_postgres/ut_select.sql
@@ -2,12 +2,14 @@
 -- Paginação com LIMIT e OFFSET
 SELECT coluna FROM tabela LIMIT 15 OFFSET 60; --15 itens, página 4 (15*4)
 
+-- Fields com case
+SELECT (case when campo_exemplo = 'teste' then 'result_1' else 'result_2' end) as teste_case_campo FROM tabela_exemplo
 
 -- SUB SELECT
-select meses.cod, meses.mes, busca.*
-from (values(1,'Jan'),(2,'Fev'),(3,'Mar')) as meses (cod,mes)
-left join (
-	select 
+SELECT meses.cod, meses.mes, busca.*
+FROM (values(1,'Jan'),(2,'Fev'),(3,'Mar')) as meses (cod,mes)
+LEFT JOIN (
+	SELECT 
 		count(faa.cod) as faa_quantidade,
 		count(faa_old.cod) as faa_quantidade_old,
 		entidade.cod,
@@ -15,14 +17,14 @@ left join (
 		faa.mes,
 		faa.ano,
 		faa_old.ano as ano_old
-	from (values(1,'ent1'),(2,'ent2')) as entidade (cod,nome)
-	left join (
+	FROM (values(1,'ent1'),(2,'ent2')) as entidade (cod,nome)
+	LEFT JOIN (
 		values(1,1,1,2014),(2,1,2,2015),(3,2,1,2014),(4,2,1,2015),(5,2,2,2014)
 		) as faa (cod,entidade,mes,ano) on faa.entidade = entidade.cod and faa.ano = 2015
-	left join (
+	LEFT JOIN (
 		values(1,1,1,2014),(2,1,2,2015),(3,2,1,2014),(4,2,1,2015),(5,2,2,2014)
 		) as faa_old (cod,entidade,mes,ano) on faa_old.entidade = entidade.cod and faa_old.mes = faa.mes and faa_old.ano = 2014
-	group by entidade.cod, entidade.nome, faa.mes, faa.ano, faa_old.mes, faa_old.ano
+	GROUP BY entidade.cod, entidade.nome, faa.mes, faa.ano, faa_old.mes, faa_old.ano
 ) as busca on busca.mes = meses.cod
 
 -- hash de um registro da tabela
