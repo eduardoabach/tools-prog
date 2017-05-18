fc2fbdc49e657c0f23206944ad3f325f6ac04c2c
eduardoabach <eduardoabach@gmail.com>
2017-05-18 14:28:02 -0300

    PHP, várias formas de sleep

--- lang_php/sleep.php
+++ lang_php/sleep.php
@@ -18,3 +18,18 @@ usleep(200000); // trabalha com milionésimos de segundos, então ($segundos * 1
     
 // de volta!
 echo date('h:i:s') . "\n";
+
+// ######################################
+
+// PHP 5~7, Para uma melhor precisão pode usar time_nanosleep, aqui 2segundo e 100 milisegundos
+$nano = time_nanosleep(2, 100000);
+if ($nano === true) {
+    echo "Esperou por 2 segundos e 100 milisegundos.";
+} elseif ($nano === false) {
+    echo "Falha na espera.";
+} elseif (is_array($nano)) {
+    $seconds = $nano['seconds'];
+    $nanoseconds = $nano['nanoseconds'];
+    echo "Interrompido por um sinal.";
+    echo "Time remanescente: $seconds segundos, $nanoseconds nanosegundos.";
+}
