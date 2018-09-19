86e5528aa72a19781d708d7132d268d998f09b03
eduardoabach <eduardoabach@gmail.com>
2018-09-19 17:21:50 -0300

    Clear reposit

--- conhecimentos gerais/bin/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -x
-cd "$(dirname ${BASH_SOURCE[0]})"/..
-echo "Starting build"
-docker run -v "$PWD":/mdn -w /mdn node:latest bash -c "/usr/local/bin/npm install && /usr/local/bin/npm run build"
-echo "Build finished"
