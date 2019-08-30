7375c486ef978b1f6bff33263c0e9819fa5c847d
Eduardo Bach <eduardoabach@gmail.com>
2019-08-30 18:45:27 -0300

    C loop probabilidade com params

--- lang_c/random-test/random_loop.c
+++ lang_c/random-test/random_loop.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <time.h>
 
-//1.000.000.000 1 Bilhão
-#define N_LIMIT_LOOP 100000
-#define N_LIMIT 10
+long N_LIMIT = 12;
+long N_LIMIT_LOOP = 10000; //1000000000 //1.000.000.000 1 Bilhão
 
 volatile sig_atomic_t stop = 0;
 void stop_by_ctrl_c(int sig){
@@ -12,7 +12,21 @@ void stop_by_ctrl_c(int sig){
 	stop = 1;
 }
 
-int main(void){
+void do_loop();
+int get_random_number();
+void text_list_result();
+void text_clear();
+
+int main(int argc, char *argv[]){
+
+	if(argc > 1){
+		N_LIMIT = strtol(argv[1], NULL, 10);
+	}
+
+	if(argc > 2){
+		N_LIMIT_LOOP = strtol(argv[2], NULL, 10);
+	}
+
 	signal(SIGINT, stop_by_ctrl_c);
 	srand(time(0));  //random variando por momento de exec do prog
 	do_loop();
@@ -21,8 +35,8 @@ int main(void){
 
 void do_loop(){
 	int n_rand;
-	int n_list[] = { [0 ... N_LIMIT] = 0 };
-
+	int n_list[N_LIMIT];
+	
 	int n_loop = 0;
 	while (!stop && n_loop < N_LIMIT_LOOP){
 		n_loop++;
@@ -63,13 +77,21 @@ void text_list_result(int n_list[]){
 		printf("%4d: %4d | %.3f(\%) | %.3f(\%) prob.\n", n_atual, n_list[i], percent, diff_probabilidade);
 	}
 
-	double media_aritmetica_probab = (double)(N_LIMIT+1) / 2; // esse exemplo nao tem o Zero, portanto soma 1 na média
-	double media_aritmetica_atingida = soma_results / N_LIMIT_LOOP;
-	double media_aritmetica_diff = media_aritmetica_probab - media_aritmetica_atingida;
+	double media_arit_probab = (double)(N_LIMIT+1) / 2; // esse exemplo nao tem o Zero, portanto soma 1 na média
+	double media_arit_atingida = soma_results / N_LIMIT_LOOP;
+	double media_arit_diff = media_arit_probab - media_arit_atingida;
+	double media_arit_diff_percent = media_arit_diff / media_arit_probab * 100;
+
 
 	printf("\n");
 	printf("Soma dos Resultados: %.3f\n", soma_results);
-	printf("Média Aritmética: %.3f / %.3f (%.3f)\n", media_aritmetica_atingida, media_aritmetica_probab, media_aritmetica_diff);
+	printf(
+		"Média Aritmética: %.3f / %.3f | %.3f = %.3f(%) \n", 
+		media_arit_atingida, 
+		media_arit_probab, 
+		media_arit_diff, 
+		media_arit_diff_percent
+	);
 	printf("Diff Probabilidade Acumulada: %.6f\n", diff_probabilidade_acumulada);
 	printf("\n");
 
